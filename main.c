c20d5430a0022506cad256d17dc4107d5854ed89
Salabrat <salabrat66@gmail.com>
2023-03-30 15:38:06 +0400

    11-2

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
-
-#include <stdio.h>
-#define ARRAY_SIZE 1000000
-int main () {
-      	printf ("ARRAY_SIZE: %d\n", ARRAY_SIZE);
-      	char v[ARRAY_SIZE];
-      	for (int i=0; i<ARRAY_SIZE; i++) {
-      	v[i]=i;}
-      	for (int i=0; i<ARRAY_SIZE; i++) {
-                	printf ("%5d", v[i]);}return 0;}
+#include <stdio.h> 
+int main (int argc, char *argv[]) {
+      	printf ("argc: %d\n", argc);
+      	printf ("argv[0]: %s", argv);
+      	return 0;}
+ 
