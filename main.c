25af415008b47e84a848f68fc8419484828862cd
Salabrat <salabrat66@gmail.com>
2023-04-04 13:23:46 +0400

    8-7

--- main.c
+++ main.c
@@ -1,20 +1,36 @@
-
 #include <stdio.h>
 
 int main() {
-    int array[5][3]; 
-    int array1[5] = {1, 2, 3, 4, 5}; 
-    int array2[5] = {6, 7, 8, 9, 10};    
- int array3[5] = {11, 12, 13, 14, 15}; 
-    for (int i = 0; i < 5; i++) {
-        array[i][0] = array1[i];
-        array[i][1] = array2[i];
-        array[i][2] = array3[i];
+    int arr1[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int arr2[3][4];
+
+    
+    int k = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            arr2[i][j] = arr1[k++];
+        }
     }
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", array[i][j]);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", arr2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    k = 0;
+    for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < 3; i++) {
+            arr2[i][j] = arr1[k++];
+        }
+    }
+
+    // вывод массива arr2
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", arr2[i][j]);
         }
         printf("\n");
     }
