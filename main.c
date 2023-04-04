19474ef35ea2c53f6519e387a6cbe09272508dcf
Salabrat <salabrat66@gmail.com>
2023-04-04 13:22:09 +0400

    8-6

--- main.c
+++ main.c
@@ -1,24 +1,22 @@
+
 #include <stdio.h>
 
 int main() {
-    int array[3][5]; 
+    int array[5][3]; 
     int array1[5] = {1, 2, 3, 4, 5}; 
-
-    int array2[5] = {6, 7, 8, 9, 10}; 
-
-    int array3[5] = {11, 12, 13, 14, 15}; 
-
+    int array2[5] = {6, 7, 8, 9, 10};    
+ int array3[5] = {11, 12, 13, 14, 15}; 
     for (int i = 0; i < 5; i++) {
-        array[0][i] = array1[i];
-        array[1][i] = array2[i];
-        array[2][i] = array3[i];
+        array[i][0] = array1[i];
+        array[i][1] = array2[i];
+        array[i][2] = array3[i];
     }
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
             printf("%d ", array[i][j]);
         }
-        printf("\n\n");
+        printf("\n");
     }
 
     return 0;
