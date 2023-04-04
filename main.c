72ff267a43a404595ca3665f909f0a15cce40411
Salabrat <salabrat66@gmail.com>
2023-04-04 13:15:52 +0400

    8-1

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
-#include <stdio.h> 
-int main (int argc, char *argv[]) {
-      	printf ("argc: %d\n", argc);
-      	printf ("argv[0]: %s", argv);
-      	return 0;}
- 
+#define ROWS 2
+#define COLS 3
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+int m[ROWS][COLS] = {{11, 12, 13},
+                     {21, 22, 33}};
+int i=0,j=0;
+int min=m[0][0],max=0;
+int i_min_index=0,j_min_index=0;
+int i_max_index=0,j_max_index=0;                  
+for (j=0;j<COLS;j++){
+  for (i=0;i<ROWS;i++){
+    if (m[i][j]>max){
+      max=m[i][j];
+      i_max_index=i;
+      j_max_index=j;}
+    if (m[i][j]<min){
+      min=m[i][j];
+      i_min_index=i;
+      j_min_index=j;} 
+    printf(" %d",m[i][j]);}
+  printf("\n");}
+printf("Maximal number is %d [%d,%d]\n",max,i_max_index,j_max_index);
+printf("Minimal number is %d [%d,%d]\n",min,i_min_index,j_min_index);}
