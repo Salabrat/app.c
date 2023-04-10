588ceb8d67a0aed7fa165f2b38999a6f8ec072f9
Salabrat <salabrat66@gmail.com>
2023-04-10 14:54:01 +0400

    18-1

--- main.c
+++ main.c
@@ -1,31 +1,73 @@
-
 #include <stdio.h>
 #include <windows.h>
 #include <tlhelp32.h>
+#include <psapi.h>
 
 int main()
 {
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (snapshot == INVALID_HANDLE_VALUE) {
-        printf("Could not create process snapshot\n");
+    HANDLE hProcess;
+    DWORD processID;
+
+    printf("Enter process ID:\n");
+    scanf("%d", &processID);
+
+    // Открытие процесса
+    hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
+
+    if (hProcess == NULL) {
+        printf("Could not open process\n");
         return 1;
     }
 
-    PROCESSENTRY32 process;
-    process.dwSize = sizeof(process);
-
-    if (!Process32First(snapshot, &process)) {
-        printf("Could not get first process\n");
-        CloseHandle(snapshot);
-        return 1;
+    // Получение имени процесса
+    char processName[MAX_PATH];
+    if (GetModuleBaseName(hProcess, NULL, processName, MAX_PATH) == 0) {
+        printf("Could not get process name\n");
+    } else {
+        printf("Process name: %s\n", processName);
     }
 
-    printf("Process List:\n");
+    // Получение ID родительского процесса
+    DWORD parentID = 0;
+    HANDLE hParent = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hParent != INVALID_HANDLE_VALUE) {
+        PROCESSENTRY32 pe32;
+        pe32.dwSize = sizeof(pe32);
+        if (Process32First(hParent, &pe32)) {
+            do {
+                if (pe32.th32ProcessID == processID) {
+                    parentID = pe32.th32ParentProcessID;
+                    break;
+                }
+            } while (Process32Next(hParent, &pe32));
+        }
+        CloseHandle(hParent);
+    }
+    if (parentID == 0) {
+        printf("Could not get parent process ID\n");
+    } else {
+        printf("Parent process ID: %d\n", parentID);
+    }
 
-    do {
-        printf("  ID: %d | Name: %s\n", process.th32ProcessID, process.szExeFile);
-    } while (Process32Next(snapshot, &process));
+    // Получение пути к исполняемому файлу процесса
+    char processPath[MAX_PATH];
+    DWORD pathSize = GetModuleFileNameEx(hProcess, NULL, processPath, MAX_PATH);
+    if (pathSize == 0) {
+        printf("Could not get process path\n");
+    } else {
+        printf("Process path: %s\n", processPath);
+    }
 
-    CloseHandle(snapshot);
+    // Получение использования памяти процессом
+    PROCESS_MEMORY_COUNTERS pmc;
+    if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
+        printf("Working Set Size: %u bytes\n", pmc.WorkingSetSize);
+    } else {
+        printf("Could not get memory info\n");
+    }
+
+    // Закрытие процесса
+    CloseHandle(hProcess);
+
     return 0;
 }
