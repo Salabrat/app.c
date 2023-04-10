ab757d461a736da7a208001c9869fabd0164e87e
Salabrat <salabrat66@gmail.com>
2023-04-10 14:45:32 +0400

    17-1

--- main.c
+++ main.c
@@ -1,27 +1,71 @@
 #include <stdio.h>
 #include <windows.h>
+#include <tlhelp32.h>
+#include <psapi.h>
 
-int main() {
-    WIN32_FIND_DATA file_data;
-    HANDLE file_handle;
+int main()
+{
+    HANDLE hProcess;
+    DWORD processID;
 
-    const char* directory_path = "C:\\Users\\calab\\Documents"; // замените на свой путь
-    file_handle = FindFirstFile(directory_path, &file_data);
+    printf("Enter process ID:\n");
+    scanf("%d", &processID);
 
-    if (file_handle == INVALID_HANDLE_VALUE) {
-        printf("Error open catalog: %d\n", GetLastError());
+    // Открытие процесса
+    hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
+
+    if (hProcess == NULL) {
+        printf("Could not open process\n");
         return 1;
     }
 
-    do {
-        printf("File name: %s\n", file_data.cFileName);
-        printf("File atribute: %d\n", file_data.dwFileAttributes);
-    } while (FindNextFile(file_handle, &file_data) != 0);
+    // Получение имени процесса
+    char processName[MAX_PATH];
+    if (GetModuleBaseName(hProcess, NULL, processName, MAX_PATH) == 0) {
+        printf("Could not get process name\n");
+    } else {
+        printf("Process name: %s\n", processName);
+    }
 
-    FindClose(file_handle);
+    
+    DWORD parentID = 0;
+    HANDLE hParent = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hParent != INVALID_HANDLE_VALUE) {
+        PROCESSENTRY32 pe32;
+        pe32.dwSize = sizeof(pe32);
+        if (Process32First(hParent, &pe32)) {
+            do {
+                if (pe32.th32ProcessID == processID) {
+                    parentID = pe32.th32ParentProcessID;
+                    break;
+                }
+            } while (Process32Next(hParent, &pe32));
+        }
+        CloseHandle(hParent);
+    }
+    if (parentID == 0) {
+        printf("Could not get parent process ID\n");
+    } else {
+        printf("Parent process ID: %d\n", parentID);
+    }
+
+    char processPath[MAX_PATH];
+    DWORD pathSize = GetModuleFileNameEx(hProcess, NULL, processPath, MAX_PATH);
+    if (pathSize == 0) {
+        printf("Could not get process path\n");
+    } else {
+        printf("Process path: %s\n", processPath);
+    }
+
+    PROCESS_MEMORY_COUNTERS pmc;
+    if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
+        printf("Working Set Size: %u bytes\n", pmc.WorkingSetSize);
+    } else {
+        printf("Could not get memory info\n");
+    }
+
+    
+    CloseHandle(hProcess);
 
     return 0;
 }
-
-
-
